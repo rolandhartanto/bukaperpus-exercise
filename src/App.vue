fb055269a0f9fcb7a403288b79e4afb09dd38859
Roland Hartanto <rolandhartanto@gmail.com>
2018-05-23 17:07:32 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue App"/>
+    <div>
+      <ul>
+        <li v-for="user in users">
+          {{user.firstname}} {{user.lastname}}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <div>Change the text </div>
+      <input v-model="teks" placeholder="What's on your mind?" />
+      <div>Test Output: 
+        <span v-text="teks"></span>
+      </div>
+    </div>
+    <div>
+      <button v-on:click="count++">You've Clicked this Button {{ count }} times!</button>
+      <button v-on:click="inc()">You've clicked this button {{ count2 }} times</button>
+    </div>
   </div>
 </template>
 
@@ -11,7 +29,24 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data () {
+    return {
+      teks: '',
+      users: [
+        {firstname: 'Sebastian', lastname: 'Eschweiler'},
+        {firstname: 'Bill', lastname: 'Smith'},
+        {firstname: 'John', lastname: 'Porter'}
+      ],
+      count: 0,
+      count2:0
+    }
+  },
+  methods:{
+    inc() {
+      this.count2+=1
+    }
   }
 }
 </script>
